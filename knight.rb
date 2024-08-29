1ebd8b125fd1741cf8754baa62f7ce1454eb6fb6
ZephyRemix <hunyaochong@gmail.com>
2024-08-29 19:59:26 +0800

    Add set of moves available to knight, and knight_moves pseudo

--- knight.rb
+++ knight.rb
@@ -3,12 +3,15 @@ require_relative "tree"
 require "pry-byebug"
 
 class Knight
-  attr_accessor 
+  attr_accessor :moves
   def initialize
-
+    self.moves = [[2,1], [1,2], [-2,1], [-1,2], [-2,-1], [-1, -2], [2,-1], [1,-2]]
   end
 
   def knight_moves(start, dest)
-
+    # create path graph
+    # search for shortest path
+    # store shortest path to shortest_path var
+    # return shortest_path
   end
 end
